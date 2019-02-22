fc3f85bd2560def9e03d67e978326bd056f6a9d8
Ponos <vlruata@gmail.com>
2019-02-22 22:37:10 +0530

    small changes

--- src/config/routes-constant/AmcRoutes.jsx
+++ src/config/routes-constant/AmcRoutes.jsx
@@ -1,5 +1,5 @@
 export class AmcRoutes {
-  static ROOT="/"
+  static ROOT = "/";
   static LOGIN = "/login";
   static HOME = "/home";
   static E_OFFICE = "/e-office";
@@ -8,9 +8,9 @@ export class AmcRoutes {
   static RENEW_ADVERTISER = "/advertiser/:id/renew";
   static ADVERTISER_DETAIL = "/advertisers/:id";
 
-  static APPLY_SHOP_LICENSE = "/shopping-license/new";
-  static RENEW_SHOP_LICENSE = "/shopping-license/:id/renew";
-  static SHOP_LICENSE_DETAIL = "/shopping-license/:id";
+  static APPLY_SHOP_LICENSE = "/shop-license/new";
+  static RENEW_SHOP_LICENSE = "/shop-license/:id/renew";
+  static SHOP_LICENSE_DETAIL = "/shop-license/:id";
 
   static APPLY_HOARDING = "/hoarding/new";
   static RENEW_HOARDING = "/hoarding/:id/renew";
@@ -21,19 +21,19 @@ export class AmcRoutes {
   static RENEW_KIOSK = "/kiosk/:id/renew";
   static KIOSK_DETAIL = "/kiosk/:id";
 
-  static APPLY_BANNER="/banner/new";
+  static APPLY_BANNER = "/banner/new";
 
   static DESK = "/e-office/desk";
   static NEW_FILE = "/e-office/file/new";
-  static CREATED_FILES = "/e-office/created";
-  static SENT_FILE = "/e-office/sent";
-  static CLOSE_FILE = "/e-office/sent";
-  static FILE_DETAIL = "/e-office/files/:id";
+  static CREATED_FILES = "/e-office/file";
+  static SENT_FILE = "/e-office/file/sent";
+  static CLOSE_FILE = "/e-office/file/close";
+  static FILE_DETAIL = "/e-office/file/:id";
 
-  static NEW_RECEIPT = "/e-office/receipts/new";
-  static CREATED_RECEIPT = "/e-office/receipts/created";
-  static SENT_RECEIPT = "/e-office/receipts/sent";
-  static CLOSE_RECEIPT = "/e-office/receipts/close";
-  static RECEIPT_DETAIL = "/e-office/receipts/:id";
+  static NEW_RECEIPT = "/e-office/receipt/new";
+  static CREATED_RECEIPT = "/e-office/receipt/created";
+  static SENT_RECEIPT = "/e-office/receipt/sent";
+  static CLOSE_RECEIPT = "/e-office/receipt/close";
+  static RECEIPT_DETAIL = "/e-office/receipt/:id";
 
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,8 +2,8 @@ import LandingPage from "../layouts/LandingPage";
 import OfficeDashboard from "../layouts/OfficeDashboard";
 
 var indexRoutes = [
-  {path: '/e-office', name:'E-office', component:OfficeDashboard },
-  {path: '/', name:'LandingPage', component:LandingPage},
+  {path: '/e-office', name:'e-Office', component: OfficeDashboard },
+  {path: '/', name:'AMC - e-Office', component: LandingPage },
 ];
 
 export default indexRoutes;
