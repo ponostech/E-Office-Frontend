48e66d03254f6e8a97661f865e9297db85023157
thangtea <riomiyachi.1991@gmail.com>
2019-02-27 15:04:40 +0530

    pdf view added

--- src/views/e-office/receipt/NewReceipt.jsx
+++ src/views/e-office/receipt/NewReceipt.jsx
@@ -1,112 +1,102 @@
 import React, { Component } from "react";
-import {
-  Button,
-  Card,
-  CardActionArea,
-  CardActions,
-  CardContent,
-  CardHeader,
-  Divider,
-  List,
-  ListItem,
-  ListItemText,
-  Paper, Tabs,
-  Typography
-} from "@material-ui/core";
+import { Button, Card, CardActionArea, CardActions, CardContent, CardHeader, Divider, Paper } from "@material-ui/core";
 import Dropzone from "react-dropzone";
-import { Tab } from "@material-ui/icons";
 import GridContainer from "../../../components/Grid/GridContainer";
 import GridItem from "../../../components/Grid/GridItem";
 import Constraint from "../../../config/Constraint";
+import PdfView from "../../../components/PdfView";
 
 class NewReceipt extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      openDialog: false,
-      selectedFiles: []
+      file: null
     };
   }
 
   onDrop = (acceptedFiles, rejectedFiles) => {
     let temp = [...this.state.selectedFiles, ...acceptedFiles];
-    this.setState({
-      selectedFiles: temp
+    acceptedFiles.forEach(file => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const readAsArrayBuffer = reader.result;
+        let f = new Blob([readAsArrayBuffer], { type: "application/pdf" });
+        let t = URL.createObjectURL(f);
+        this.setState({ file: f });
+      };
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
+
+      reader.readAsArrayBuffer(file);
     });
-    console.log(this.state.selectedFiles);
+
   };
 
-  handleBrowseDialog = () => {
-    this.setState({
-      openDialog: false
-    });
-  };
+  getView = () => {
+    if (this.state.file) {
+      return (
+        <Dropzone
+          multiple={false}
+          accept={Constraint.ACCEPTED_DOCUMENTS}
+          onDrop={this.onDrop}>
+          {({ getRootProps, getInputProps, isDragActive }) => {
+            return (
+              <Paper style={{ padding: 50 }}
+                     {...getRootProps()}
+                // className={classNames('dropzone', {'dropzone--isActive': isDragActive})}
+              >
+                <input {...getInputProps()} />
+                {
+                  isDragActive ?
+                    <p>Drop files here...</p> :
+                    <p>Try dropping some files here, or click to select files to
+                      upload.</p>
+                }
+              </Paper>
+            );
+          }}
+        </Dropzone>
+      );
+    } else {
+      return (
+        <div>
+          <PdfView title={"File name"} file={this.state.file}/>
+          <Button variant={"outlined"} color={"secondary"}>Cancel</Button>
+        </div>
 
-  handleBrowse = () => {
-    this.setState({
-      openDialog: true
-    });
+      );
+    }
   };
 
   render() {
-    const items =
-      this.state.selectedFiles.map(file => {
-        return (
-          <ListItem key={file.name}>
-            <ListItemText color={"danger"} primary={file.name}/>
-          </ListItem>
-        );
-      });
 
     return (
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={6}>
-            <Card>
-              <CardHeader title={"New receipt"}/>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={6}>
+          <Card>
+            <CardHeader title={"New receipt"}/>
 
-              <CardContent>
-                <Dropzone
-                  accept={Constraint.ACCEPTED_DOCUMENTS}
-                  onDrop={this.onDrop}>
-                  {({ getRootProps, getInputProps, isDragActive }) => {
-                    return (
-                      <Paper style={{ padding: 50 }}
-                             {...getRootProps()}
-                        // className={classNames('dropzone', {'dropzone--isActive': isDragActive})}
-                      >
-                        <input {...getInputProps()} />
-                        {
-                          isDragActive ?
-                            <p>Drop files here...</p> :
-                            <p>Try dropping some files here, or click to select files to
-                              upload.</p>
-                        }
-                      </Paper>
-                    );
-                  }}
-                </Dropzone>
-                <Typography variant={"title"}>List of selected file</Typography>
-                <Divider/>
-                <List>
-                  {items}
-                </List>
-                <Divider/>
-              </CardContent>
-              <CardActionArea>
-                <CardActions title={"Upload"}/>
-              </CardActionArea>
-            </Card>
-          </GridItem>
-          <GridItem xs={12} sm={12} md={6}>
+            <CardContent>
+              <Divider/>
+              {this.getView()}
+              <Divider/>
 
-            <Button variant={"extendedFab"} color={"primary"} >Create and Generate Receipt
-              No</Button>
-            <Button style={{ marginTop: 10 }} variant={"outlined"} color={"secondary"}>
-              reset
-            </Button>
-          </GridItem>
-        </GridContainer>
+            </CardContent>
+            <CardActionArea>
+              <CardActions title={"Upload"}/>
+            </CardActionArea>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={6}>
+
+          <Button variant={"extendedFab"} color={"primary"}>Create and Generate Receipt
+            No</Button>
+          <Button style={{ marginTop: 10 }} variant={"outlined"} color={"secondary"}>
+            reset
+          </Button>
+        </GridItem>
+      </GridContainer>
     );
   }
 }
