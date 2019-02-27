9a9916853711bf9a735c70156abc4928337d0fb3
Ponos <vlruata@gmail.com>
2019-02-27 21:54:46 +0530

    rearrange code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+import {createBrowserHistory} from "history";
+import {Route, Router, Switch} from "react-router-dom";
 import indexRoutes from "routes/index.jsx";
 import NoMatch from "./views/NoMatch";
-import "assets/scss/material-dashboard-pro-react.css?v=1.4.0";
+
+import "assets/scss/material-dashboard-pro-react.scss?v=1.5.0";
 
 const hist = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} component={prop.component} key={key} />;
-      })}
-      <Route component={NoMatch} />
-    </Switch>
-  </Router>,
-  document.getElementById("root")
+    <Router history={hist}>
+        <Switch>
+            {indexRoutes.map((prop, key) => {
+                return <Route path={prop.path} component={prop.component} key={key}/>;
+            })}
+            <Route component={NoMatch}/>
+        </Switch>
+    </Router>,
+    document.getElementById("root")
 );
