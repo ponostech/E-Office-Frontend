7e777020c7ba2e97c40eeb76d245d10ac64f8df0
thangtea <riomiyachi.1991@gmail.com>
2019-02-28 11:31:39 +0530

    file

--- src/views/model/NewFileViewModel.js
+++ src/views/model/NewFileViewModel.js
@@ -1,15 +1,19 @@
 export class NewFileViewModel {
-  static TITLE="New File";
-  static SECONDARY_BTN_TEXT="Cancel";
-  static PRIMARY_BTN_TEXT="Save";
-  static FILENO_LABEL="File No";
-  static SUBJECT_LABEL="Subject";
-  static DEALER_LABEL="Dealer";
-  static CATEGORY_LABEL="Category";
-  static CLASSIFICATION_LABEL="Classification";
-  static REMARK_LABEL="Remark";
-  static PREVIOUS_LABEL="Previous Ref";
-  static NEXT_LABEL="Next Ref";
+  static TITLE = "New File";
+  static SECONDARY_BTN_TEXT = "Cancel";
+  static PRIMARY_BTN_TEXT = "Save";
+  static FILENO_LABEL = "File No";
+  static SUBJECT_LABEL = "Subject";
+  static DEALER_LABEL = "Dealer";
+  static CATEGORY_LABEL = "Category";
+  static CLASSIFICATION_LABEL = "Classification";
+  static REMARK_LABEL = "Remark";
+  static PREVIOUS_LABEL = "Previous Ref";
+  static NEXT_LABEL = "Next Ref";
+  static CATEGORY_PLACEHOLDER = "Category";
+  static SUBTITLE=" Star marks are mandatory";
+  static REQUIRED_FILENO = "File No is required";
+  static REQUIRED_SUBJECT="Subject is required";
 
 
 }
