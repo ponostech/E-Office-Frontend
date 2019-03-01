75b1793f23518848459102e1443701429b25fedb
thangtea <riomiyachi.1991@gmail.com>
2019-03-01 16:33:13 +0530

    staff ready to test

--- src/config/ApiRoutes.jsx
+++ src/config/ApiRoutes.jsx
@@ -1,6 +1,7 @@
 export class ApiRoutes {
   static LOGIN_ROUTE = "/auth/login";
   static  BASE_URL = "http://139.59.26.3/api/v1";
-  static LOCAL_COUNCIL="/local-councils";
+  static LOCAL_COUNCIL = "/local-councils";
+  static CREATE_STAFF = "/register/staff";
 
 }
