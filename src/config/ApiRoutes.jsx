ea1268c3547a4e4a0d76920ed078347d8c272fbc
thangtea <riomiyachi.1991@gmail.com>
2019-03-01 14:04:16 +0530

    unkn

--- src/config/ApiRoutes.jsx
+++ src/config/ApiRoutes.jsx
@@ -1,4 +1,6 @@
 export class ApiRoutes {
-  static LOGIN_ROUTE = "/login";
+  static LOGIN_ROUTE = "/auth/login";
+  static  BASE_URL = "http://139.59.26.3/api/v1";
+  static LOCAL_COUNCIL="/local-councils";
 
 }
