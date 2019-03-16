a11fabe5bc5050bbb00c25ff134cc916dbaea3f8
dingnghetafanai <dingnghetafanai@gmail.com>
2019-03-16 13:59:20 +0530

    Banner update

--- src/views/banner/BannerApplicationForm.jsx
+++ src/views/banner/BannerApplicationForm.jsx
@@ -10,61 +10,65 @@ import loginPageStyle from "../../assets/jss/material-dashboard-pro-react/views/
 import withStyles from "@material-ui/core/styles/withStyles";
 import GridContainer from "../../components/Grid/GridContainer";
 import ImageUpload from "../../components/CustomUpload/ImageUpload";
-import { StaffService } from "../../services/StaffService";
 import SubmitDialog from "../../components/SubmitDialog";
 import OfficeSnackbar from "../../components/OfficeSnackbar";
 
 class BannerApplicationForm extends Component {
 
-  //staffService = new StaffService();
+  // staffService = new StaffService();
 
   state = {
-        name: "",
-        phone_no: "",
-        applicant_type: "",
-        local_council_id: "",
-        address: "",
-        ownership: {value: "private", label: "Private"},
-        display_type:  {value: "vehicle", label: "Vehicle"},
-        display_on: "",
-        locations: "",
-        length: "",
-        height: "",
-        no_advertisements: "",
-        establishmentDate: "",
-        from: "",
-        to:"",
-        latitude: 0,
-        longitude: 0,
-        details:"",
-        status:"",
-        signature:"",
-        localCouncils: ["one", "two", "three"],
-        ownerships: [
-        {value: "private", label: "Private"},
-        {value: "firm", label: "Firm"},
-        {value: "company", label: "Company"},
-        {value: "charitable trust", label: "Charitable Trust"},
-        {value: "others", label: "Others"}
-      ],
-        display_types: [
-        {value: "vehicle", label: "Vehicle"},
-        {value: "umbrella", label: "Umbrella"},
-        {value: "balloons", label: "Balloons"},
-        {value: "video", label: "Video"},
-        {value: "audio/sound", label: "Audio/Sound"}
+    name: "",
+    ownership: null,
+    address: "",
+    display_type: null,
+    dob: "1991/12/12",
+    blood: "",
+    nameError: "",
+    addressError: "",
+    dobError: "",
+    signature: null,
+    name: "",
+    phone_no: "",
+    applicant_type: "",
+    local_council_id: "",
+    address: "",
+    advertisement_type: "",
+    display_on: "",
+    locations: "",
+    length: "",
+    height: "",
+    from: "",
+    to: "",
+    latitude: 0,
+    longitude: 0,
+    details: "",
+    status: "",
+    display_types: [
+      { value: "vehicle", label: "Vehicle" },
+      { value: "umbrella", label: "Umbrella" },
+      { value: "balloons", label: "Balloons" },
+      { value: "video", label: "Video" },
+      { value: "audio/sound", label: "Audio/Sound" },
+      { value: "others", label: "Others" }
     ],
-        openDialog: false,
-        submit: false,
-        complete: false,
-        attachments: []
+    ownerships: [
+      { value: "private", label: "Private" },
+      { value: "firm", label: "Firm" },
+      { value: "company", label: "Company" },
+      { value: "charitable trust", label: "Charitable Trust" },
+      { value: "others", label: "Others" }
+    ],
+    submit: false,
+    complete: false,
+    attachments: []
 
   };
 
   componentDidMount() {
-    document.title = "e-AMC | Banners/Posters ApplicationForm";
-    this.state.ownerships = this.state.ownerships[0];
-    //this.state.branch = this.state.branches[0];
+    document.title = "e-AMC | Banners/Posters Application Form";
+    this.state.ownership = this.state.ownerships[0];
+    this.state.display_type = this.state.display_types[0];
   }
 
   handleChange = (e) => {
@@ -76,13 +80,12 @@ class BannerApplicationForm extends Component {
 
   handleSelect = (identifier, value) => {
     switch (identifier) {
-      case "ownerships":
+      case "ownership":
         this.setState({ ownership: value });
         break;
-      case "display_types":
+      case "display_type":
         this.setState({ display_type: value });
         break;
-
       default:
         break;
 
@@ -108,7 +111,7 @@ class BannerApplicationForm extends Component {
     this.setState({ submit: true });
     this.staffService.create(this.state)
       .then(res => {
-        this.setState({complete:true })
+        this.setState({ complete: true });
         console.log(res);
       })
       .then(() => {
@@ -120,15 +123,17 @@ class BannerApplicationForm extends Component {
     const name = e.target.name;
     switch (name) {
       case "primary":
-          this.onSubmit();
+        this.onSubmit();
         break;
       case "secondary":
         this.setState({
           name: "",
           address: "",
-          dob: '',
+          phone_no: "",
           blood: "",
-          signature:null,
+          ownership: this.state.ownerships[0],
+          display_type: this.state.display_types[0],
+          signature: null
         });
         break;
       default:
@@ -155,7 +160,10 @@ class BannerApplicationForm extends Component {
       case "address":
         value.length === 0 ? this.setState({ addressError: BannerViewModel.ADDRESS_REQUIRED }) : this.setState({ addressError: "" });
         break;
-        default:
+      case "phone_no":
+        value.length === 0 ? this.setState({ phoneError: BannerViewModel.PHONE_REQUIRED }) : this.setState({ phoneError: "" });
+        break;
+      default:
         break;
     }
   };
@@ -163,125 +171,125 @@ class BannerApplicationForm extends Component {
 
   render() {
     const { ownership } = this.props;
-    const { dispay_type } = this.props;
+    const { display_type } = this.props;
     const { classes } = this.props;
 
     return (
-      <div className={classes.container}>
-         <GridItem xs={12} sm={12} md={10}>
-            <form>
-              <Card style={{padding:50}}>
-                <CardHeader title={BannerViewModel.TILE} subheader={BannerViewModel.SUBHEADER}/>
-                <SubmitDialog open={this.state.submit} text={BannerViewModel.SUBMIT}/>
-                <OfficeSnackbar variant={"success"} open={this.state.complete} message={BannerViewModel.CREATE_MESSAGE} />
-                <CardBody>
-                  <GridContainer>
-                    <GridItem md={6} xs={12}>
-                  <TextField
-                    value={this.state.name}
-                    ref={"nameRef"}
-                    name={"name"}
-                    onBlur={this.handleBlur.bind(this)}
-                    required={true}
-                    variant={"outlined"}
-                    margin={"dense"}
-                    fullWidth={true}
-                    onChange={this.handleChange.bind(this)}
-                    label={BannerViewModel.NAME}
-                    error={Boolean(this.state.nameError)}
-                    helperText={this.state.nameError}
-                  />
-                    </GridItem>
-                    <GridItem md={6} xs={12}>
-                  <OfficeSelect
-                    variant={"outlined"}
-                    margin={"dense"}
-                    value={this.state.ownership}
-                    fullWidth={true}
-                    name={"application_type"}
-                    onChange={this.handleSelect.bind(this, "ownership")}
-                    ClearAble={true}
-                    label={BannerViewModel.APPLICANT_TYPE}
-                    options={this.state.ownerships}/>
-                    </GridItem>
-                  </GridContainer>
-                  <GridContainer>
-                    <GridItem md={6} xs={12}>
-                  <TextField
-                    value={this.state.address}
-                    name={"address"}
-                    onBlur={this.handleBlur.bind(this)}
-                    required={true}
-                    multiline={true}
-                    rows={3}
-                    variant={"outlined"}
-                    margin={"dense"}
-                    fullWidth={true}
-                    error={Boolean(this.state.addressError)}
-                    helperText={this.state.addressError}
-                    onChange={this.handleChange.bind(this)}
-                    label={BannerViewModel.ADDRESS}/>
-                    </GridItem>
-                    <GridItem md={6} xs={12}>
-                  <OfficeSelect value={this.state.display_type}
-                                label={BannerViewModel.DISPLAY_TYPE}
-                                name={"display_type"}
-                                variant={"outlined"}
-                                margin={"dense"}
-                                fullWidth={true}
-                                onChange={this.handleSelect.bind(this, "display_type")}
-                                options={this.state.display_types}/>
-                    </GridItem>
-                  </GridContainer>
-                    <GridContainer>
-                      <GridItem md={6} xs={12}>
-                  <TextField name={"dob"}
-                             value={this.state.dob}
-                             variant={"outlined"}
-                             margin={"dense"}
-                             required={true}
-                             fullWidth={true}
-                             onChange={this.handleChange.bind(this)}
-                             type={"date"}
-                             InputLabelProps={
-                               { shrink: true }
-                             }
-                             label={BannerViewModel.DOB}
-                             error={Boolean(this.state.dobError)}
-                             helperText={this.state.dobError}
-                  />
-                      </GridItem>
-                    </GridContainer>
-                  {/*{this.getAttachmentView()}*/}
 
-                  <Divider/>
-                  <ImageUpload setImagePreviewUrl={this.setImagePreviewUrl.bind(this)}
-                               onFileSelect={this.onSignatureSelect.bind(this)}
-                               onRemove={this.onSignatureRemove.bind(this)} label={BannerViewModel.SIGNATURE}/>
-                  {/*<DocumentsDropzone documents={[*/}
-                  {/*{ name: "Staff signature", fileName: "signature" }*/}
-                  {/*]}*/}
-                  {/*openDialog={this.state.openDialog} onCloseHandler={this.handleDocuments.bind(this)}*/}
-                  {/*acceptedFiles={Constraint.ACCEPTED_IMAGES}/>*/}
+      <GridContainer justify="center">
+        <GridItem xs={12} sm={12} md={10}>
+          <form>
+            <Card style={{ padding: 50 }}>
+              <CardHeader title={BannerViewModel.TITLE} subheader={BannerViewModel.SUB_HEADER}/>
+              <SubmitDialog open={this.state.submit} text={BannerViewModel.SUBMIT}/>
+              <OfficeSnackbar variant={"success"} open={this.state.complete}
+                              message={BannerViewModel.CREATE_MESSAGE}/>
+              <CardBody>
 
-                </CardBody>
-                <CardActions>
-                  <Button name={"primary"} disabled={this.state.submit}
-                          color={"primary"} variant={"outlined"}
-                          onClick={this.handleClick.bind(this)}>
-                    {BannerViewModel.PRIMARY_TEXT}
-                  </Button>
-                  <Button name={"secondary"}
-                          color={"secondary"}
-                          variant={"outlined"}
-                          onClick={this.handleClick.bind(this)}>
-                    {BannerViewModel.SECONDARY_TEXT}
-                  </Button>
-                </CardActions>
-              </Card>
-            </form>
-          </GridItem>
-         </div>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <TextField
+                      value={this.state.name}
+                      ref={"nameRef"}
+                      name={"name"}
+                      onBlur={this.handleBlur.bind(this)}
+                      required={true}
+                      variant={"outlined"}
+                      margin={"dense"}
+                      fullWidth={true}
+                      onChange={this.handleChange.bind(this)}
+                      label={BannerViewModel.NAME}
+                      error={Boolean(this.state.nameError)}
+                      helperText={this.state.nameError}
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <OfficeSelect
+                      variant={"outlined"}
+                      margin={"dense"}
+                      value={this.state.ownership}
+                      fullWidth={true}
+                      name={"applicant_type"}
+                      onChange={this.handleSelect.bind(this, "ownership")}
+                      ClearAble={true}
+                      label={BannerViewModel.APPLICANT_TYPE}
+                      options={this.state.ownerships}/>
+                  </GridItem>
+
+                  <GridItem xs={12} sm={12} md={6}>
+                    <TextField
+                      value={this.state.phone_no}
+                      ref={"phoneRef"}
+                      name={"phone_no"}
+                      variant={"outlined"}
+                      margin={"dense"}
+                      fullWidth={true}
+                      onChange={this.handleChange.bind(this)}
+                      label={BannerViewModel.PHONE_NO}/>
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <OfficeSelect value={this.state.display_type}
+                                  label={BannerViewModel.DISPLAY_TYPE}
+                                  name={"display_on"}
+                                  variant={"outlined"}
+                                  margin={"dense"}
+                                  fullWidth={true}
+                                  onChange={this.handleSelect.bind(this, "display_type")}
+                                  options={this.state.display_types}/>
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <TextField
+                      value={this.state.address}
+                      name={"address"}
+                      onBlur={this.handleBlur.bind(this)}
+                      required={true}
+                      multiline={true}
+                      rows={3}
+                      variant={"outlined"}
+                      margin={"dense"}
+                      fullWidth={true}
+                      error={Boolean(this.state.addressError)}
+                      helperText={this.state.addressError}
+                      onChange={this.handleChange.bind(this)}
+                      label={BannerViewModel.ADDRESS}/>
+                  </GridItem>
+                </GridContainer>
+
+                {/*{this.getAttachmentView()}*/}
+                <GridItem xs={12} sm={12} md={6}>
+                <Divider/>
+                <ImageUpload setImagePreviewUrl={this.setImagePreviewUrl.bind(this)}
+                             onFileSelect={this.onSignatureSelect.bind(this)}
+                             onRemove={this.onSignatureRemove.bind(this)} label={BannerViewModel.SIGNATURE}/>
+                {/*<DocumentsDropzone documents={[*/}
+                {/*{ name: "Staff signature", fileName: "signature" }*/}
+                {/*]}*/}
+                {/*openDialog={this.state.openDialog} onCloseHandler={this.handleDocuments.bind(this)}*/}
+                {/*acceptedFiles={Constraint.ACCEPTED_IMAGES}/>*/}
+                </GridItem>
+              </CardBody>
+              <GridItem xs={12} sm={12} md={6}>
+              <CardActions>
+
+                <Button name={"primary"} disabled={this.state.submit}
+                        color={"primary"} variant={"outlined"}
+                        onClick={this.handleClick.bind(this)}>
+                  {BannerViewModel.PRIMARY_TEXT}
+                </Button>
+                <Button name={"secondary"}
+                        color={"secondary"}
+                        variant={"outlined"}
+                        onClick={this.handleClick.bind(this)}>
+                  {BannerViewModel.SECONDARY_TEXT}
+                </Button>
+              </CardActions>
+              </GridItem>
+
+            </Card>
+          </form>
+        </GridItem>
+      </GridContainer>
+
     );
   }
 
