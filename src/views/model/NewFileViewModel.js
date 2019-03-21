eead2e03eb5abf1dd9608e359f22508f2f4ebe7f
Ponos <vlruata@gmail.com>
2019-03-21 14:37:02 +0530

    tables

--- src/views/model/NewFileViewModel.js
+++ src/views/model/NewFileViewModel.js
@@ -1,17 +1,17 @@
 export class NewFileViewModel {
-  static TITLE = "New File";
+  static TITLE = "Create New File";
   static SECONDARY_BTN_TEXT = "Cancel";
   static PRIMARY_BTN_TEXT = "Save";
-  static FILENO_LABEL = "File No";
+  static FILE_NO_LABEL = "File No";
   static SUBJECT_LABEL = "Subject";
-  static DEALER_LABEL = "Dealer";
-  static CATEGORY_LABEL = "Category";
+  static DEALER_LABEL = "Dealing Staff";
+  static CATEGORY_LABEL = "Branch";
   static CLASSIFICATION_LABEL = "Classification";
   static REMARK_LABEL = "Remark";
   static PREVIOUS_LABEL = "Previous Ref";
   static NEXT_LABEL = "Next Ref";
   static CATEGORY_PLACEHOLDER = "Category";
-  static SUBTITLE=" Star marks are mandatory";
+  static SUBTITLE="* are mandatory";
   static REQUIRED_FILENO = "File No is required";
   static REQUIRED_SUBJECT="Subject is required";
 
