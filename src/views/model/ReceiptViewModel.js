d60c22e5c3c73e32bf9c3e9a94437a2f4bc40adb
dingnghetafanai <dingnghetafanai@gmail.com>
2019-03-21 16:11:07 +0530

    Receipt Detail Updated

--- src/views/model/ReceiptViewModel.js
+++ src/views/model/ReceiptViewModel.js
@@ -5,9 +5,14 @@ export default class ReceiptViewModel {
   static EMAIL = "Email";
   static PASSWORD = "Password";
   static CONFIRM_PASSWORD = "Confirm Password";
-  static ADDRESS = "Address";
-  static APPLICANT_TYPE = "Type of applicant";
+  static ADDRESS = "Sender Address";
+  static DEALING_HAND = "Dealing Hand";
   static SIGNATURE="Signature";
+  static SUBJECT="Subject";
+  static TYPE="Type";
+  static LANGUAGE="Language";
+  static CATEGORY="Select Category";
+  static REF_NO="Letter Reference No";
 
   static DOC_ATTACHMENT = "Document attachment";
   static APPLICANT_DETAIL = "Applicant Details";
@@ -21,8 +26,10 @@ export default class ReceiptViewModel {
   static MIN_PASSWORD = "Minimum character of password must be 7";
   static MATCH_PASSWORD = "Confirm password must match password";
   static PHONE_ERROR = "Phone number must be 10 digit no";
+  static REQUIRED_SUBJECT = "Please fill in subject";
   static REQUIRED_ADDRESS = "Address is required";
   static RECEIVE_DATE = "Receive Date";
   static LETTER_DATE = "Letter Date"
+  static DELIVERY_MODE = "Delivery Mode"
 
 };
