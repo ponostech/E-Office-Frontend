9ca06192d4c7ef1720a1211f076aa7279aea4cc8
thangtea <riomiyachi.1991@gmail.com>
2019-04-30 16:35:09 +0530

    login update

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,14 +1,22 @@
 import axios from "axios";
 import { ApiRoutes } from "../config/ApiRoutes";
+import { ErrorToString } from "../utils/ErrorUtil";
 
 export default class AuthService {
 
-  async getUser(id){
 
-  }
-
-  async login(email, password) {
-    return await axios.post(ApiRoutes.LOGIN_ROUTE,{email,password});
+  async login(email, password,errorCallback,successCallback) {
+    try{
+      let res=await axios.post(ApiRoutes.LOGIN_ROUTE,{email,password});
+      if (res.data.status) {
+        successCallback(res)
+      }else{
+        errorCallback(res.data.messages);
+      }
+    }catch (e) {
+      console.error(e)
+      errorCallback("Something went wrong: Please try again later")
+    }
   }
 
 };
