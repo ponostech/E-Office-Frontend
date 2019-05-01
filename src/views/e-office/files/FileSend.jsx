213afd270e6a9e69b313f59c8707fc75900ef283
Ponos <vlruata@gmail.com>
2019-05-01 22:21:55 +0530

    send file axios + menu bug fix ;

--- src/views/e-office/files/FileSend.jsx
+++ src/views/e-office/files/FileSend.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import OfficeSelect from "../../../components/OfficeSelect";
 import {ApiRoutes} from "../../../config/ApiRoutes";
+import {DESK} from "../../../config/routes-constant/OfficeRoutes";
 import Grid from "@material-ui/core/Grid";
 import {Button, Card, CardHeader, DialogActions} from "@material-ui/core";
 import CardContent from "@material-ui/core/CardContent";
@@ -10,40 +11,57 @@ import Divider from "@material-ui/core/Divider";
 class FileSend extends Component {
     state = {
         staffs: [],
-        recipientId: null,
+        recipient_id: null,
     };
 
     componentDidMount() {
-        console.log(this.props.doLoad);
         this.props.doLoad(true);
         this.getStaffs();
     }
 
-    getStaffs() {
+    getStaffs = () => {
         axios.get(ApiRoutes.GET_STAFF)
             .then(res => {
-                let staffs = FileSend.formatStaff(res.data.data.staffs);
+                let staffs = this.formatStaff(res.data.data.staffs);
                 this.setState({staffs: staffs});
                 this.props.doLoad(false);
             })
             .catch(err => {
                 this.props.doLoad(false);
             })
-    }
+    };
 
-    static formatStaff(staffs) {
+    formatStaff = (staffs) => {
         return staffs.map(obj => {
             let temp = {};
             temp['value'] = obj.id;
             temp['label'] = obj.staff.name + " (" + obj.staff.designation + ")";
             return temp;
         });
-    }
+    };
 
     handleOfficeSelect = (identifier, value) => {
         this.setState({
             [identifier]: value
         });
+    };
+
+    handleSubmit = () => {
+        const {history} = this.props;
+        if (this.state.recipient_id) {
+            this.props.doLoad(false);
+            let data = {
+                recipient_id: this.state.recipient_id.value,
+            };
+
+            axios.post("/file/"+ this.props.file.id + "/send", data)
+                .then(res => {
+                    this.props.doLoad(false);
+                    history.push(DESK);
+                })
+        } else {
+            console.log('Error');
+        }
     };
 
     render() {
@@ -53,22 +71,21 @@ class FileSend extends Component {
                 <CardContent>
                     <Grid item xs={6}>
                         <OfficeSelect
-                            value={this.state.recipientId}
+                            value={this.state.recipient_id}
                             options={this.state.staffs}
-                            name={"to"}
+                            name={"recipient_id"}
                             label={"Send File To"}
                             variant={"outlined"}
                             margin={"dense"}
                             required={true}
                             fullWidth={true}
-                            onChange={this.handleOfficeSelect.bind(this, "recipientId")}
+                            onChange={this.handleOfficeSelect.bind(this, "recipient_id")}
                         />
                     </Grid>
                 </CardContent>
                 <Divider/>
                 <DialogActions>
-                    <Button color="primary">Send File</Button>
-                    <Button color="secondary">Cancel</Button>
+                    <Button color="primary" onClick={this.handleSubmit.bind(this)}>Send File</Button>
                 </DialogActions>
             </>
         )
