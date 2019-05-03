664b1445f34f4c0353abdff214cf87d39f7dc123
ruata <vlruata@gmail.com>
2019-05-03 14:25:53 +0530

    active list

--- src/views/e-office/files/notesheet/Notesheet.jsx
+++ src/views/e-office/files/notesheet/Notesheet.jsx
@@ -2,56 +2,30 @@ import React, {Component} from "react";
 import axios from 'axios';
 import moment from 'moment';
 import {EventNote} from "@material-ui/icons";
+import {CardHeader, Divider} from "@material-ui/core";
 import Timeline from "../../../../components/Timeline/Timeline.jsx";
 import DefaultAvatar from "../../../../assets/img/default-avatar.png";
 import CreateNoteButton from "./NotesheetCreateButton";
 import CreateNoteDialog from "./NoteCreateDialog";
-import {CardHeader} from "@material-ui/core";
 import {FILE_NOTESHEET} from "../../../../config/ApiRoutes";
-
-const widgetStories = [
-    {
-        inverted: true,
-        badgeColor: "success",
-        badgeIcon: EventNote,
-        title: "",
-        titleColor: "info",
-        avatar: DefaultAvatar,
-        body: (
-            <>
-                <p>
-                    Your products, all the kits that I have downloaded from your site and worked with are sooo cool! I love the color mixtures, cards... everything. Keep up the great work!
-                </p>
-                <p>
-                    Your products, all the kits that I have downloaded from your site and worked with are sooo cool! I love the color mixtures, cards... everything. Keep up the great work!
-                </p>
-            </>
-        ),
-        footerName: "Alec Thompson",
-        footerDesignation: "Town Planner",
-        footerTitle: (
-            <div>
-                Dated: 3rd February, 2019 (Tuesday)
-            </div>
-        )
-    },
-];
 
 class Notesheets extends Component {
     state = {
         note: [],
-        openDialog: false
+        openDialog: false,
+        loading: true,
     };
 
     componentDidMount() {
         axios.get(FILE_NOTESHEET(this.props.file.id))
             .then(res => {
-                console.log('Note:', res.data);
                 let noteSheet = res.data;
                 if(noteSheet.status) {
                     this.formatNote(noteSheet.data.notesheets);
+                    this.setState({loading: false});
                 } else {
-                    console.log("Fail: ", noteSheet)
+                    console.log("Fail Notesheet: ", noteSheet);
+                    this.setState({loading: false});
                 }
             });
     }
@@ -72,7 +46,6 @@ class Notesheets extends Component {
             return temp;
         });
         this.setState({note: formattedNote});
-        console.log('format:', formattedNote);
     };
 
     handleOpenCreateNote = () => {
@@ -87,6 +60,8 @@ class Notesheets extends Component {
         return (
                 <>
                     <CardHeader title={"File No.: " + this.props.file.number} subheader={"Subject: " + this.props.file.subject}/>
+                    <Divider/>
+                    <br/>
                     <CreateNoteButton click={this.handleOpenCreateNote} />
                     <Timeline simple stories={this.state.note} />
                     <CreateNoteButton click={this.handleOpenCreateNote} />
