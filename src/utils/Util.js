94ea43480e3b1b9dd880f41e8414f4749ee255e0
thangtea <riomiyachi.1991@gmail.com>
2019-05-09 13:36:27 +0530

    few change to hoarding kiosk banner newfile

---
+++ src/utils/Util.js
@@ -0,0 +1,18 @@
+
+export const FILE_STATUS ={
+  NEW:"new",
+  INACTIVE:"in-active"
+}
+
+export const FormatStaff = (staffs) => {
+  const user_id = JSON.parse(localStorage.getItem('current_user')).id;
+  return staffs.filter(function (obj) {
+    return obj.id !== user_id;
+  })
+    .map(obj => {
+      let temp = {};
+      temp['value'] = obj.id;
+      temp['label'] = obj.staff.name + " (" + obj.staff.designation + ")";
+      return temp;
+    });
+};
