10b7a2f8b75d2a6f71700dab6066e6be98e616b7
ruata <vlruata@gmail.com>
2019-05-10 13:35:01 +0530

    editor height

--- src/views/e-office/files/draft/Editor.jsx
+++ src/views/e-office/files/draft/Editor.jsx
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 import Editor from "nib-core";
 
+export const styles = {
+  wrapper: {
+    height: "250px",
+  }
+};
+
 class TextEditor extends Component {
   render() {
     return (
-          <Editor onChange={this.props.onChange} defaultValue={this.props.default}/>
+        <Editor theme={styles} onChange={this.props.onChange} defaultValue={this.props.default}/>
     );
   }
 }
