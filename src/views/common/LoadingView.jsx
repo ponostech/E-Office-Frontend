cb02ea0603f194bb63468911dbd904c41ed97578
thangtea <riomiyachi.1991@gmail.com>
2019-06-01 11:23:56 +0530

    package.json

--- src/views/common/LoadingView.jsx
+++ src/views/common/LoadingView.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import {CircularProgress, Grid} from "@material-ui/core";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import { CircularProgress, Grid } from "@material-ui/core";
 
 const loadingView = (props) => {
-    return (
-        <Grid container justify={"center"} spacing={32}>
-            <Grid item lg={12} align={props.align ? props.align : 'center'}>
-                {/*<div style={{margin: 20}}>*/}
-                {/*    <CircularProgress variant={"indeterminate"}*/}
-                {/*                      color={props.color ? props.color : "primary"}/>*/}
-                {/*</div>*/}
-                <Skeleton count={38}/>
-            </Grid>
-        </Grid>
-    );
+  return (
+    <Grid container justify={"center"} spacing={32}>
+      <Grid item lg={12} align={props.align ? props.align : "center"}>
+        <div style={{ margin: 20 }}>
+          <CircularProgress variant={"indeterminate"}
+                            color={props.color ? props.color : "primary"}/>
+        </div>
+        {/*<Skeleton count={38}/>*/}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default loadingView;
