6758ff479cc6f1fe24b07586d951a2cb66c2e54f
thangtea <riomiyachi.1991@gmail.com>
2019-06-01 11:38:39 +0530

    update

--- src/views/landing-pages/HomePage.jsx
+++ src/views/landing-pages/HomePage.jsx
@@ -16,8 +16,6 @@ import {
     Grievance,
     ShopLicensing
 } from "./Services/Services";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import Typography from "@material-ui/core/Typography";
 import { authContext } from "../../context/AuthContext";
 
@@ -104,42 +102,6 @@ const styles = {
     }*/
 };
 
-/*var settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    initialSlide: 0,
-
-    prevArrow: <PrevIcon fontSize={"large"} color={"primary"}/>,
-    nextArrow: <NextIcon fontSize={"large"} color={"primary"}/>,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                initialSlide: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
-};*/
 
 class HomePage extends Component {
     handleLink = link => event => {
