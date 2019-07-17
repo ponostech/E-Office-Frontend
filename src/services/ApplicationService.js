a7ec9d8df6c1217556d283e5a013489f09864246
thangtea <riomiyachi.1991@gmail.com>
2019-07-17 22:39:48 +0530

    approve update:

--- src/services/ApplicationService.js
+++ src/services/ApplicationService.js
@@ -38,7 +38,23 @@ class ApplicationService {
       errorCallback(e.toString())
     }
   }
-
+  async getDraft(id,params,errorCallback,successCallback){
+    try{
+      let res=await axios.get(`drafts/${id}`,{params})
+      if (res.data.status) {
+          successCallback(res.data.data.draft)
+      }else{
+        if (res.data.validation_error) {
+          errorCallback(ErrorToString(res.data.messages));
+        } else {
+          errorCallback(ArrayToString(res.data.messages))
+        }
+      }
+    } catch (e) {
+      console.error(e)
+      errorCallback(e.toString())
+    }
+  }
 
 }
 
