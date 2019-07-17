f45fd824900fa6321033e024125bd8fd1686fea7
thangtea <riomiyachi.1991@gmail.com>
2019-07-17 23:38:23 +0530

    impose fine

--- src/views/common/ApplicationDetailsDialog.jsx
+++ src/views/common/ApplicationDetailsDialog.jsx
@@ -2,20 +2,20 @@ import React from "reactn";
 import { Button, Divider, List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
 import DialogWrapper from "../e-office/files/dialog/common/DialogWrapper";
 import Grid from "@material-ui/core/Grid";
-import moment from "moment";
 import { AttachFile } from "@material-ui/icons";
 import ListSubheader from "@material-ui/core/ListSubheader";
 import ApplicationService from "../../services/ApplicationService";
 import SendMessageDialog from "../e-office/files/dialog/common/SendMessageDialog";
 import SubmitDialog from "../../components/SubmitDialog";
-import OfficeSnackbar from "../../components/OfficeSnackbar";
 import ApplicationResolver from "../e-office/files/dialog/common/ApplicationResolver";
+import ImposedFineDialog from "../e-office/files/dialog/ImposedFineDialog";
 
 class ApplicationDetailsDialog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       openMessage: false,
+      openFine: false,
       submit: false,
       submitTitle: "Submit",
 
@@ -35,18 +35,28 @@ class ApplicationDetailsDialog extends React.Component {
     console.log(msg);
   };
 
+  imposeFine = (data) => {
+    this.setState({ openFine: false });
+    this.setState({ submit: true, submitTitle: "Impose Fine" });
+    this.applicationService.imposeFine(data,
+      errorMsg => this.setGlobal({ errorMsg }),
+      successMsg => this.setGlobal({ successMsg }))
+      .finally(() => this.setState({ submit: false }));
+  };
+
   render() {
     const { application, open, title, type, onClose } = this.props;
-    const { openMessage, submit, submitTitle, errorMessage, successMessage } = this.state;
+    const { openMessage, submit, submitTitle,openFine, errorMessage, successMessage } = this.state;
 
     const action = <>
       <Button onClick={onClose} color='primary'>Approve Application</Button>
       <Button onClick={e => this.setState({ openMessage: true })} color='primary'>Send Message</Button>
+      <Button onClick={e => this.setState({ openFine: true })} color='primary'>Impose Fine</Button>
       <Button onClick={onClose} color='secondary'>Close</Button>
     </>;
-    let LEFT_ITEMS = ApplicationResolver(application)
+    let LEFT_ITEMS = ApplicationResolver(application);
 
-    let leftRow = LEFT_ITEMS.map(val => <DetailViewRow key={val.key} primary={val.key} secondary={val.value}/>);
+    let leftRow = LEFT_ITEMS.map(val => <DetailViewRow key={val.key} primary={val.name} secondary={val.value}/>);
 
     let documents = application.documents ? application.documents.map(val => <FileViewRow key={val.id}
                                                                                           data={val}/>) : [];
@@ -68,11 +78,8 @@ class ApplicationDetailsDialog extends React.Component {
         <SubmitDialog open={submit} text={"Please wait..."} title={submitTitle}/>
         <SendMessageDialog application={application} open={openMessage}
                            onClose={e => this.setState({ openMessage: false })} onMessageSend={this.sendMessage}/>
-
-        <OfficeSnackbar open={Boolean(errorMessage)} onClose={e => this.setState({ errorMessage: "" })}
-                        variant={"error"} message={errorMessage}/>
-        <OfficeSnackbar open={Boolean(successMessage)} onClose={e => this.setState({ errorMessage: "" })}
-                        variant={"error"} message={successMessage}/>
+        <ImposedFineDialog onClose={() => this.setState({ openFine: false })} open={openFine} file={this.props.file}  application={application}
+                           imposeFine={this.imposeFine}/>
       </>
     );
   }
